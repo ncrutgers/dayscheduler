5916ff98676d1134c332314f35435152b7cff718
Willow <61257976+ncrutgers@users.noreply.github.com>
2020-04-07 03:19:56 -0400

    added assets, README, & made changes to script, html, & css

---
+++ README.md
@@ -0,0 +1,13 @@
+# Third-Party APIs: Work Day Scheduler
+
+A calendar application that allows the user to save events for each hour of the day.
+
+## Criteria
+
+* Use of the [Moment.js](https://momentjs.com/) library to work with date and time.
+* Opening the planner, the current day is displayed at the top of the calendar.
+* Timeblocks for standard business hours (9AM-5PM) for that day
+* Timeblocks are color coded to indicate whether it is in the past, present, or future
+* Timeblocks present a textarea where user can enter a new event
+* Clicking the save button for that timeblock, then text for that event is saved in local storage
+* Refreshing the page does not clear saved and/or entered text in textarea
--- script.js
+++ script.js
@@ -1,3 +1,92 @@
+// set current day
 
-$("#currentDay").text(new Date().toDateString());
+$("#currentDay").text(moment().format('dddd, MMMM Do'));
+//$("#currentDay").text(new Date().toDateString());
+
+// create a variable that stores current hour
+var currentHour = moment().format('h');
+
+// added classes for styling to all spans & buttons
+$("span").addClass("hour");
+$("button").addClass("saveBtn");
+
+
+// when a timeblock is a specific time then change to a color: red(present), yellow(past), & green(future)
+$("span").each(function(){
+    if (parseInt(this.title) === currentHour){
+        $(this).parent().next().addClass("present");
+    } else if (parseInt(this.title) < currentHour){
+        $(this).parent().next().addClass("past");
+    } else if (parseInt(this.title) > currentHour){
+        $(this).parent().next().addClass("future");
+    }
+    
+  });
+
+
+// when refreshing the page events are not cleared
+window.onload = function (event){
+    if (this.localStorage != null){
+        $(".event1").val(this.localStorage.getItem("event1"));
+        $(".event2").val(this.localStorage.getItem("event2"));
+        $(".event3").val(this.localStorage.getItem("event3"));
+        $(".event4").val(this.localStorage.getItem("event4"));
+        $(".event5").val(this.localStorage.getItem("event5"));
+        $(".event6").val(this.localStorage.getItem("event6"));
+        $(".event7").val(this.localStorage.getItem("event7"));
+        $(".event8").val(this.localStorage.getItem("event8"));
+        $(".event9").val(this.localStorage.getItem("event9"));
+    }
+};  
+
+// Event listener added to timeblock buttons; events saved to local storage
+
+$(".submit1").on("click", function(event){
+    event.preventDefault();      
+    localStorage.setItem("event1", $(".event1").val());
+});
+
+$(".submit2").on("click", function(event){
+    event.preventDefault();    
+    localStorage.setItem("event2", $(".event2").val());
+});
+
+$(".submit3").on("click", function(event){
+    event.preventDefault();    
+    localStorage.setItem("event3", $(".event3").val());
+});
+
+$(".submit4").on("click", function(event){
+    event.preventDefault();
+    localStorage.setItem("event4", $(".event4").val());
+});
+
+$(".submit5").on("click", function(event){
+    event.preventDefault();
+    localStorage.setItem("event5", $(".event5").val());
+});
+
+$(".submit6").on("click", function(event){
+    event.preventDefault();
+    localStorage.setItem("event6", $(".event6").val());
+});
+
+$(".submit7").on("click", function(event){
+    event.preventDefault();
+    localStorage.setItem("event7", $(".event7").val());
+});
+
+$(".submit8").on("click", function(event){
+    event.preventDefault();
+    localStorage.setItem("event8", $(".event9").val());
+});
+
+$(".submit9").on("click", function(event){
+    event.preventDefault();
+    localStorage.setItem("event9", $(".event9").val());
+});
+
+
+
+
 
